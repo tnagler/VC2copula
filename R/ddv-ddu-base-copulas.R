121d0fbe9be9480b7f44a90455635f188a99f91c
tnagler <thomas.nagler@tum.de>
2024-01-15 12:00:50 +0100

    missed a few

--- R/ddv-ddu-base-copulas.R
+++ R/ddv-ddu-base-copulas.R
@@ -10,7 +10,6 @@ setMethod("ddvCopula", signature("matrix", "normalCopula"), BiCopHfunc2.copula)
 setMethod("ddvCopula", signature("numeric", "normalCopula"),
           function(u, copula, ...)
             BiCopHfunc2.copula(matrix(u, ncol = copula@dimension), copula))
-
 #' @rdname ddCopula
 setMethod("dduCopula", signature("matrix", "tCopula"), BiCopHfunc1.copula)
 #' @rdname ddCopula
@@ -23,7 +22,6 @@ setMethod("ddvCopula", signature("matrix", "tCopula"), BiCopHfunc2.copula)
 setMethod("ddvCopula", signature("numeric", "tCopula"),
           function(u, copula, ...)
             BiCopHfunc2.copula(matrix(u, ncol = copula@dimension), copula))
-
 #' @rdname ddCopula
 setMethod("dduCopula", signature("matrix", "gumbelCopula"), BiCopHfunc1.copula)
 #' @rdname ddCopula
@@ -36,8 +34,9 @@ setMethod("ddvCopula", signature("matrix", "gumbelCopula"), BiCopHfunc2.copula)
 setMethod("ddvCopula", signature("numeric", "gumbelCopula"),
           function(u, copula, ...)
             BiCopHfunc2.copula(matrix(u, ncol = copula@dimension), copula))
-
+#' @rdname ddCopula
 setMethod("dduCopula", signature("matrix", "claytonCopula"), BiCopHfunc1.copula)
+#' @rdname ddCopula
 setMethod("dduCopula", signature("numeric", "claytonCopula"),
           function(u, copula, ...)
             BiCopHfunc1.copula(matrix(u, ncol = copula@dimension), copula))
@@ -47,8 +46,9 @@ setMethod("ddvCopula", signature("matrix", "claytonCopula"), BiCopHfunc2.copula)
 setMethod("ddvCopula", signature("numeric", "claytonCopula"),
           function(u, copula, ...)
             BiCopHfunc2.copula(matrix(u, ncol = copula@dimension), copula))
-
+#' @rdname ddCopula
 setMethod("dduCopula", signature("matrix", "indepCopula"), BiCopHfunc1.copula)
+#' @rdname ddCopula
 setMethod("dduCopula", signature("numeric", "indepCopula"),
           function(u, copula, ...)
             BiCopHfunc1.copula(matrix(u, ncol = copula@dimension), copula))
@@ -58,7 +58,6 @@ setMethod("ddvCopula", signature("matrix", "indepCopula"), BiCopHfunc2.copula)
 setMethod("ddvCopula", signature("numeric", "indepCopula"),
           function(u, copula, ...)
             BiCopHfunc2.copula(matrix(u, ncol = copula@dimension), copula))
-
 #' @rdname ddCopula
 setMethod("dduCopula", signature("matrix", "frankCopula"), BiCopHfunc1.copula)
 #' @rdname ddCopula
